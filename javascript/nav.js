b060a231cee3c753f2c47655e819ca62ef469f86
yusha.kuralay <yusha.kuralay@arksoft.com.tr>
2024-08-28 14:49:00 +0300

    ++ nav css fixed ++ nav.js apth changed

--- javascript/nav.js
+++ javascript/nav.js
@@ -1,8 +1,8 @@
-const nav = document.querySelector(".nav");
+const nav = document.getElementById("nav");
 window.addEventListener("scroll", fixNav);
 
 function fixNav() {
-  if (window.scrollY > nav.offsetHeight + 150) {
+  if (window.scrollY > nav.offsetHeight) {
     nav.classList.add("active");
   } else {
     nav.classList.remove("active");
